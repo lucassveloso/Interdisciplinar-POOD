990ba5261e42f0b8dcdc3e0a0195c52b00367ada
lucassveloso <lucassveloso@hotmail.com>
2015-07-09 19:36:10 -0300

    Adicionando o script do banco

---
+++ scriptInterdisciplinar.sql
@@ -0,0 +1,17 @@
+insert into Etnias values (1,"Branco");
+insert into Etnias values (2,"Negro");
+insert into Etnias values (3,"Mulato");
+insert into Etnias values (4,"Indígena");
+
+
+insert into Religiao values (1,"Católica");
+insert into Religiao values (2,"Evangélica");
+insert into Religiao values (3,"Espírita");
+insert into Religiao values (4,"Umbanda");
+insert into Religiao values (5,"Candomblé");
+insert into Religiao values (6,"Judaísmo");
+
+insert into Sexo values (1,"Masculino");
+insert into Sexo values (2,"Feminino");
+insert into Sexo values (3,"Homem Trans.");
+insert into Sexo values (4,"Mulher Trans.");
