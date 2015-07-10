e8f2988c425e414728860f5313ceb5625a6336f5
Lucas Veloso <lucas.s.veloso@hotmail.com>
2015-07-10 19:22:26 -0300

    Atualizando Script

--- scriptInterdisciplinar.sql
+++ scriptInterdisciplinar.sql
@@ -1,17 +1,21 @@
-insert into Etnias values (1,"Branco");
-insert into Etnias values (2,"Negro");
-insert into Etnias values (3,"Mulato");
-insert into Etnias values (4,"Indígena");
-
-
-insert into Religiao values (1,"Católica");
-insert into Religiao values (2,"Evangélica");
-insert into Religiao values (3,"Espírita");
-insert into Religiao values (4,"Umbanda");
-insert into Religiao values (5,"Candomblé");
-insert into Religiao values (6,"Judaísmo");
-
 insert into Sexo values (1,"Masculino");
 insert into Sexo values (2,"Feminino");
 insert into Sexo values (3,"Homem Trans.");
 insert into Sexo values (4,"Mulher Trans.");
+
+insert into etnias (Id_etnia,Descricao) values(1,'Brancos');
+insert into etnias (Id_etnia,Descricao) values(2,'Negros');
+insert into etnias (Id_etnia,Descricao) values(3,'Mulatos');
+insert into etnias (Id_etnia,Descricao) values(4,'Indígenas');
+insert into etnias (Id_etnia,Descricao) values(5,'Caboclos');
+insert into etnias (Id_etnia,Descricao) values(6,'Cafuzo');
+
+insert into Religiao (Id_religiao,Descricao) values(1,'Cristianismo ');
+insert into Religiao (Id_religiao,Descricao) values(2,'Islamismo ');
+insert into Religiao (Id_religiao,Descricao) values(3,'Hinduísmo ');
+insert into Religiao (Id_religiao,Descricao) values(4,'Budismo ');
+insert into Religiao (Id_religiao,Descricao) values(5,'Xintoísmo ');
+insert into Religiao (Id_religiao,Descricao) values(6,'Sikhismo');
+insert into Religiao (Id_religiao,Descricao) values(7,'Judaísmo');
+insert into Religiao (Id_religiao,Descricao) values(8,'Espiritismo');
+insert into Religiao (Id_religiao,Descricao) values(9,'Wicca');
